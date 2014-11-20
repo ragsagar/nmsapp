b137e08ec25925bd1ec09579a179ef868e280c03
Rag Sagar.V <ragsagar@gmail.com>
2014-11-20 09:30:51 +0400

    Setup.py and Manifest files for package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nmsapp',
+      version='0.1.1',
+      packages=['nms'],
+      include_package_data=True,
+      description='Nms app',
+      url='http://deam.ae',
+      author='Alya Alshamsi, Rag Sagar.V',
+      author_email='alya@deam.ae, rsagar@deam.ae',)
