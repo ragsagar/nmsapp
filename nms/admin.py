0e961d6ff8823275c9a3315c932e28486dafaa67
Rag Sagar.V <ragsagar@gmail.com>
2014-11-20 15:45:58 +0400

    Enabling admin for other models

--- nms/admin.py
+++ nms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Station, Meter, MeterInfo
+from .models import (Station, Meter, MeterInfo, Log, MeterLocation,
+        StationLocation, Mode, MeterConfig, StationStatus)
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
@@ -14,4 +15,24 @@ class MeterAdmin(admin.ModelAdmin):
 
 @admin.register(MeterInfo)
 class MeterInfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MeterLocation)
+class MeterLocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(StationLocation)
+class StationLocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MeterConfig)
+class MeterConfigAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Mode)
+class ModeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(StationStatus)
+class StationStatusAdmin(admin.ModelAdmin):
     pass
