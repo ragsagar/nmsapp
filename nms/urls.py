05e56f297469d36a01baaaf9edf6765b48449ccb
Rag Sagar.V <ragsagar@gmail.com>
2014-11-23 09:16:35 +0400

    Urls for listing meter info and modes

--- nms/urls.py
+++ nms/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
 from .views import (StationListView, MeterListView, MeterDetailView,
-        LogListView)
+        LogListView, MeterInfoListView, ModeListView)
 
 
 urlpatterns = patterns('',
-        url(r'^$',
+        url(r'^stations/$',
             StationListView.as_view(), name='list_stations'),
         url(r'^stations/(?P<pk>\d+)/meters/$',
             MeterListView.as_view(), name='station_meters_list'),
@@ -13,6 +13,10 @@ urlpatterns = patterns('',
             MeterDetailView.as_view(), name='meter_detail'),
         url(r'^logs/$',
             LogListView.as_view(), name='list_logs'),
+        url(r'^meter_infos/$',
+            MeterInfoListView.as_view(), name='list_meter_infos'),
+        url(r'^modes/$',
+            ModeListView.as_view(), name='list_modes'),
         url(r'^accounts/login/$',
             'django.contrib.auth.views.login', name='login'),
         url(r'^accounts/logout/$',
