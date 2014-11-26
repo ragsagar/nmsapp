490355f41a880b43d57e53895c16eb9ec9caee95
Rag Sagar.V <ragsagar@gmail.com>
2014-11-26 07:03:43 +0400

    Enabling admin for tower and well

--- nms/admin.py
+++ nms/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from .models import (Station, Meter, MeterInfo, Log, MeterLocation,
-        StationLocation, Mode, MeterConfig, StationStatus)
+                     StationLocation, Mode, MeterConfig, StationStatus,
+                     Tower, Well)
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
@@ -17,22 +18,37 @@ class MeterAdmin(admin.ModelAdmin):
 class MeterInfoAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(MeterLocation)
 class MeterLocationAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(StationLocation)
 class StationLocationAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(MeterConfig)
 class MeterConfigAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Mode)
 class ModeAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(StationStatus)
 class StationStatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tower)
+class TowerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Well)
+class WellAdmin(admin.ModelAdmin):
     pass
