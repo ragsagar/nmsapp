7b415ced8df173213e08f5a372580924f494fb43
Rag Sagar.V <ragsagar@gmail.com>
2014-11-26 08:24:14 +0400

    Tests for tower detail page

--- nms/tests/test_views.py
+++ nms/tests/test_views.py
@@ -2,11 +2,21 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
 
+from nms.models import Tower
 
 class ViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = self.create_user()
+
+    def create_tower(self, xc, yc, gx, gy, wd):
+        data = {'y_coordinate': yc,
+                'grid_x': gx,
+                'water_depth': wd,
+                'x_coordinate': xc,
+                'grid_y': gy}
+        return Tower.objects.create(**data)
+        
 
     def test_tower_list(self):
         url = reverse_lazy('tower_list')
@@ -24,8 +34,10 @@ class ViewTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def dtest_tower_detail(self):
-        url = reverse_lazy('tower_detail')
+    def test_tower_detail(self):
+        data = {'xc': 10, 'yc': 20, 'gx': 30, 'gy': 40, 'wd': 50}
+        tower = self.create_tower(**data)
+        url = reverse_lazy('tower_detail', kwargs={'pk': tower.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.client.login(username='ragsagar', password='password')
