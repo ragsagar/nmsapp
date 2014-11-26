4a0e5d4af314ca22bdd36efa53a033cc3c47bb63
Rag Sagar.V <ragsagar@gmail.com>
2014-11-26 11:44:07 +0400

    Function to help check status, start and stop nms

---
+++ nms/utils.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def is_nms_running():
+    """ Check if the LinuxNMS command is running or not.
+    Returns True if nms is running."""
+    command = ['ps', '-C', 'LinuxNMS']
+    returncode = subprocess.call(command)
+    if returncode == 0:
+        return True
+    else:
+        return False
+
+
+def start_nms():
+    """ Command to start LinuxNMS. Returns True if nms is started."""
+    command = ['/etc/init.d/LinuxNMS', 'start']
+    returncode = subprocess.call(command)
+    if returncode == 0:
+        return True
+    else:
+        return False
+
+
+def stop_nms():
+    """ Command to stop LinuxNMS. Return True if nms is stopped."""
+    command = ['/etc/init.d/LinuxNMS', 'stop']
+    returncode = subprocess.call(command)
+    if returncode == 0:
+        return True
+    else:
+        return False
